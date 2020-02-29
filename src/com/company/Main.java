4ed4542294daf075829967123ba1f1c196b50394
Kasia <superkasia3@gmail.com>
2020-02-29 13:52:05 +0100

    sout

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
 
     public static void main(String[] args) {
+        System.out.println("teraz sprawdzam");
         mFigury.add(new Szescian(1));
         mFigury.add(new Szescian(4));
         mFigury.add(new Szescian(3));
